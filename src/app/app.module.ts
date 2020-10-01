b0bd51f4fad997cdf4ff6e506f2e58782ba7dd65
Rodgersouko <gabrielaouko@gmail.com>
2020-10-01 10:56:44 +0300

    create service and styled header and search component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SearchComponent } from './search/search.component';
+import { DataComponent } from './data/data.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SearchComponent
+    SearchComponent,
+    DataComponent
   ],
   imports: [
     BrowserModule
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
